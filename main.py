4177b30787e43efba97ac1787e516337b0a7e1d5
Steven Kessler <stvnksslr@gmail.com>
2019-04-01 17:46:29 -0400

    feat(dowmloader):

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ smiley_source = requests.get("https://forums.somethingawful.com/misc.php?action=
 soup = BeautifulSoup(smiley_source.content, "html.parser")
 emote_folder = Path.cwd() / "emotes"
 
-test = soup.findAll("li", {"class": "smilie"})
-list_of_emotes = [smilie.contents for smilie in test]
+all_classes_with_emotes = soup.findAll("li", {"class": "smilie"})
+list_of_emotes = [emote.contents for emote in all_classes_with_emotes]
 
-dict_of_emotes = {}
+parsed_emotes_dict = {}
 
 for emote in list_of_emotes:
     emote_name = emote[3].attrs.get('title')
     emote_url = emote[3].attrs.get('src')
     emote_command = emote[1].text
-    dict_of_emotes.update({emote_name: emote_url})
+    parsed_emotes_dict.update({emote_name: emote_url})
 
-for key, value in dict_of_emotes.items():
+for key, value in parsed_emotes_dict.items():
     emote = requests.get(value)
     open(emote_folder / key, 'wb').write(emote.content)
